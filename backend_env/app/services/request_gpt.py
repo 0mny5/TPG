8dc90a857b9902095ec46bedcfe5b74d12fcdd89
0mny5 <adel_chrono_48@yahoo.co.jp>
2025-12-28 19:30:58 +0900

    Improved: プロンプト生成速度の改善

--- backend_env/app/services/request_gpt.py
+++ backend_env/app/services/request_gpt.py
@@ -17,6 +17,7 @@ def request_gpt(
         params.atmosphere,
         params.colors
     ]
+    tags = ", ".join(tags)
     quality_tags = [
         "(masterpiece,best quality:1.2)",
         "high resolution",
@@ -28,6 +29,7 @@ def request_gpt(
         "intricate details",
         "(detailed background:1.4),(glitter:1.2)"
     ]
+    quality_tags = ", ".join(quality_tags)
     fixed_negative_prompt = [
         "(easynegative:1.0)",
         "(worst quality,low quality:1.2)",
@@ -42,12 +44,13 @@ def request_gpt(
         "underexposed",
         "verybadimagenegative_v1.3",
     ]
+    fixed_negative_prompt = ", ".join(fixed_negative_prompt)
     if any(word.lower() in story.lower() for word in forbidden_words.split(',')):
         raise HTTPException(400, "Invalid story content")
     if len(story) > MAX_STORY_CHARS:
         raise HTTPException(400, "Story is too long")
 
-    inputs = "inputs:\n" + "- story:\n" + story + "\n" + "- tags:\n" + ", ".join(tags) + "\n" + "- quality_tags:\n" + ", ".join(quality_tags) + "\n" + "- fixed_negative_prompt:\n" + ", ".join(fixed_negative_prompt) + "\n\n"
+    inputs = "inputs:\n" + "- story:\n" + story + "\n" + "- tags:\n" + tags + "\n" + "- quality_tags:\n" + quality_tags + "\n" + "- fixed_negative_prompt:\n" + fixed_negative_prompt + "\n\n"
     gpt_input = inputs + base_prompt
 
     response = client.responses.create(
@@ -61,5 +64,8 @@ def request_gpt(
         assert "negativePrompt" in data
     except:
         raise HTTPException(400, "Invalid response format.")
+
+    data["prompt"] = data["prompt"] + ", " + quality_tags
+    data["negativePrompt"] = fixed_negative_prompt + ", " + data["negativePrompt"]
 
     return data
