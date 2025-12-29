63f09a4bde4d1da4cd9d09d70554d4d21932d500
0mny5 <adel_chrono_48@yahoo.co.jp>
2025-12-29 18:48:02 +0900

    Improved: googleログインユーザー検証部分を関数に切り出し

--- backend_env/app/main.py
+++ backend_env/app/main.py
@@ -2,13 +2,12 @@ from fastapi import FastAPI, Header, HTTPException
 from config import get_settings  # config.pyから設定をインポート
 from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
-from google.oauth2 import id_token
-from google.auth.transport import requests as google_requests
 from redis.asyncio import Redis
 from schemas.request import Params
 from schemas.user import User
 from services.request_gpt import request_gpt
 from services.count_generating import increment_ai_generate_count
+from services.verify_login_user import verify_login_user
 
 # 設定の取得（New）
 settings = get_settings()
@@ -40,28 +39,7 @@ async def create_prompt(
     params: Params,
     authorization: str = Header(None),
 ):
-    # 1. Headerに無ければ reject
-    if not authorization:
-        raise HTTPException(status_code=401, detail="Missing Authorization header")
-
-    # 2. Bearer 形式以外は reject
-    if not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Invalid Authorization format")
-
-    token = authorization.replace("Bearer ", "")
-
-    if not token:
-        raise HTTPException(401, "id_token missing")
-
-    # Google署名・aud・exp 検証
-    payload = id_token.verify_oauth2_token(
-        token,
-        google_requests.Request(),
-        settings.google_client_id,
-    )
-
-    sub = payload["sub"]
-
+    sub = verify_login_user(settings.google_client_id, authorization)
     generate_count = await increment_ai_generate_count(r, sub, settings.daily_limit)
 
     response = request_gpt(params, gpt_client, settings.gpt_prompt, settings.forbidden_words)
